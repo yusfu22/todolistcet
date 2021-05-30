91f6d48f70aba28bd24d82896b8aabf2cb111191
yusuf <tr.yusufpolat@gmail.com>
2021-05-31 01:24:27 +0300

    vol6

--- CetToDoList/Models/SearchViewModel.cs
+++ CetToDoList/Models/SearchViewModel.cs
@@ -7,6 +7,8 @@ namespace CetToDoList.Models
     {
         public string SearchText { get; set; }
         public bool ShowAll { get; set; }
+        public bool DesSearch { get; set; }
         public List<Todo> Result { get; set;  }
+        public List<Catagory> Abcd { get; set; }
     }
 }
